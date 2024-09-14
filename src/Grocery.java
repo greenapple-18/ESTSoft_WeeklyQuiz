75ca448861ca5265398b14d78d6e7e288ee26474
greenapple <dla16253745@gmail.com>
2024-09-14 23:53:00 +0900

    [Add] Add ProductType Class to define product categories

---
+++ src/Grocery.java
@@ -0,0 +1,7 @@
+import java.math.BigDecimal;
+
+public class Grocery extends Product {
+    public Grocery(String name, BigDecimal price, double weight) {
+        super(name, price, weight);
+    }
+}
