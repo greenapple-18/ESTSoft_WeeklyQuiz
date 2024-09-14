0582caee4001c3797c4ec97d34388fe7842a0004
greenapple <dla16253745@gmail.com>
2024-09-14 23:56:34 +0900

    [Add] Add Cart Class to calculate total delievery fees

---
+++ src/Cart.java
@@ -0,0 +1,37 @@
+import java.math.BigDecimal;
+
+public class Cart {
+    Product[] product;
+
+    Cart(Product[] product) {
+        this.product = product;
+    }
+
+    public int calculateDeliveryCharge() {
+        int deliveryFees;
+
+        BigDecimal totalPrice = new BigDecimal(0);
+        int totalWeight = 0;
+
+        for (Product value : product) {
+            totalPrice = totalPrice.add(value.getPrice().subtract(value.getDiscountAmount()));
+            totalWeight += value.getWeight();
+        }
+
+        if (totalWeight < 3) {
+            deliveryFees = 1000;
+        } else if (totalWeight < 10) {
+            deliveryFees = 5000;
+        } else {
+            deliveryFees = 10000;
+        }
+
+        if (totalPrice.compareTo(new BigDecimal("30000")) >= 0 && totalPrice.compareTo(new BigDecimal("100000")) < 0) {
+            deliveryFees -= 1000;
+        } else if (totalPrice.compareTo(new BigDecimal("100000")) >= 0) {
+            deliveryFees = 0;
+        }
+
+        return deliveryFees;
+    }
+}
