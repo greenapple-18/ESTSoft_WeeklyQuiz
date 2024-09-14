ac215da74a1f920eb04eca6dbc7b7cfb7d4f2117
greenapple <dla16253745@gmail.com>
2024-09-14 23:50:19 +0900

    [Add] Add Main Class to run the application

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        Product beauty = new Beauty("beauty", new BigDecimal(30000), 2);
+        Product grocery = new Grocery("grocery", new BigDecimal(20000), 3);
+        Product largeAppliance = new LargeAppliance("largeAppliance", new BigDecimal(50000), 5);
+
+        Cart cart = new Cart(new Product[] {beauty, grocery, largeAppliance});
+        int totalDeliveryCharge = cart.calculateDeliveryCharge();
+        System.out.println(totalDeliveryCharge);
+    }
+}
